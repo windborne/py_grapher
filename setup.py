e32e312fc1366171efbabb4fb0ff5918b1094e1a
Kai Marshland <kaimarshland@gmail.com>
2020-02-04 12:18:45 -0800

    Initialized package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py_grapher',
+    version='0.0.1',
+    description='Python wrapper for windborne graphing library',
+    url='git@github.com:windborne/py_grapher.git',
+    author='Kai Marshland',
+    author_email='kai@windbornesystems.com',
+    license='unlicense',
+    packages=['py_grapher'],
+    zip_safe=False
+)
